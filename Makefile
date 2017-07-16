9897acb7b559a65fde1cfae29273aaf9d9b566c4
Gustavo Passos <gustavo4passos@gmail.com>
2017-07-16 02:20:40 -0300

    Added glew and opengl

--- Makefile
+++ Makefile
@@ -2,13 +2,15 @@ OBJS = main.cpp
 
 CC = g++
 
-INCLUDE_PATHS = -IC:\DevLibs\SDL2-2.0.5\x86_64-w64-mingw32\include
+INCLUDE_PATHS = -IC:\DevLibs\SDL2-2.0.5\x86_64-w64-mingw32\include -IC:\DevLibs\VS\glew-2.0.0\glew-2.0.0\include
 
-LIBRARY_PATHS = -IC:\DevLibs\SDL2-2.0.5\x86_64-w64-mingw32\lib
+LIBRARY_PATHS = -IC:\DevLibs\SDL2-2.0.5\x86_64-w64-mingw32\lib -LC:\DevLibs\VS\glew-2.0.0\glew-2.0.0\lib\Release\Win32
 
-LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2
+COMPILER_FLAGS = -w -Wl, -subsystem,windows
 
-OBJ_NAME = Git
+LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2 -lglew32 -lOpenGL32
+
+OBJ_NAME = bin\Git
 
 all : $(OBJS)
-	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(LINKER_FLAGS) -o $(OBJ_NAME)
+	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(CONPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
--- main.cpp
+++ main.cpp
@@ -1,26 +1,91 @@
 #include "stdio.h"
 #include <SDL2/SDL.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 using namespace std;
 
+const int WIDTH = 640, HEIGHT = 480;
 SDL_Window* window;
 SDL_GLContext context;
 
-bool init()
+  bool init(const char* title, int windowWidth, int windowHeight, SDL_GLContext* glContext)
 {
+  //Check if SDL was initialized correctly (SDL_Init returns a negative value in case it was not able to initialize SDL)
   if( SDL_Init(SDL_INIT_VIDEO) < 0 )
-  {
-
-  }
-  else
   {
     printf("Unable to initialize SDL. Error: %s\n", SDL_GetError());
     return false;
+  }
+  else
+  {
+    printf("SDL successfully initialized.\n");
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, SDL_WINDOW_OPENGL);
+
+    if(window == NULL)
+    {
+      printf("Unable to create window. Error:%s\n", SDL_GetError());
+    }
+    else
+    {
+      *glContext = SDL_GL_CreateContext(window);
+
+      if(context == NULL)
+      {
+        printf("Unable to create OpenGL context from SDL. Error: %s", SDL_GetError());
+      }
+      else
+      {
+        glewExperimental = GL_TRUE;
+        GLenum glewErr = glewInit();
+
+        if(glewErr != GLEW_NO_ERROR)
+        {
+          printf("Unable to initialize Glew. Error: %s\n", glewGetErrorString(glewErr));
+          return false;
+        }
+      }
+    }
   }
 }
 
 int main(int argc, char** args)
 {
+  init("Git I love You", WIDTH, HEIGHT, &context);
+
+  bool running = true;
+  while(running)
+  {
+      SDL_Event e;
+      while(SDL_PollEvent(&e) != 0)
+      {
+        if(e.type == SDL_QUIT)
+        {
+          running = false;
+        }
+        else if(e.type == SDL_KEYUP)
+        {
+          switch (e.key.keysym.sym)
+          {
+            case SDLK_ESCAPE:
+            running = false;
+            break;
+
+            default:
+            break;
+          }
+        }
+      }
+
+      SDL_GL_SwapWindow(window);
+  }
 
   return 0;
 }
