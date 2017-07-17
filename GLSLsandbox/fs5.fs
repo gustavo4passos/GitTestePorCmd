f187bc541b2a8c89404275046bdef67588260a6c
Gustavo Passos <gustavo4passos@gmail.com>
2017-07-17 02:19:29 -0300

    Added new fs

--- GLSLsandbox/fs5.fs
+++ GLSLsandbox/fs5.fs
@@ -10,9 +10,9 @@ uniform float time;
 uniform vec2 mouse;
 uniform vec2 resolution;
 
-float random (in vec2 st) { 
+float random (in vec2 st) {
     return fract(sin(dot(st.xy,
-                         vec2(12.9898,78.233))) 
+                         vec2(12.9898,78.233)))
                 * 43758.5453123);
 }
 
@@ -22,16 +22,16 @@ float noise( in vec2 p )
 {
     vec2 i = floor( p );
     vec2 f = fract( p );
-    
+
     vec2 u = f*f*(5.0-2.0*f);
 
-    return mix( mix( random( i + vec2(0.0,1.0) ), 
+    return mix( mix( random( i + vec2(0.0,1.0) ),
                      random( i + vec2(1.0,0.0) ), u.x),
-                mix( random( i + vec2(0.0,1.0) ), 
+                mix( random( i + vec2(0.0,1.0) ),
                      random( i + vec2(1.0,1.0) ), u.x), u.y);
 }
 
-// Author: 
+// Author:
 void main( void ) {
 
 	vec2 p = ( gl_FragCoord.xy * 2. - resolution.xy ) / min(resolution.x, resolution.y);
@@ -45,12 +45,12 @@ void main( void ) {
 			q *= mat2(noise(vec2(time*.1)*1.));
 			q -= (p.y+y) + (p.x + x);
 			q = mod( p+q, .2) + 0.15;
-			
-			
+
+
 			color += 0.0008/length( q - (p+q) * (p-q) ) - .00001;
 		}
 	}
-	
+
 
 	gl_FragColor = vec4( vec3( color*.75, color*.9, color ), 1.0 );
 
